96d0552bfd8a90cebac1145faba0399ce3c6e1bb
Никита Васин <cskovec22@yandex.ru>
2023-07-23 20:04:35 +0700

    Add function "run_screensaver" from module "graphic_arts.start_game_banner" and add condition "if __name__ == "__main__"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     attack_value: int = 0
@@ -114,7 +116,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
 
